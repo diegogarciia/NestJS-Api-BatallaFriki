ee1e033b7fea60e7a7ccd5d66e620fd83f9cf358
Diego <diegogarciaangel25@gmail.com>
2026-02-15 21:59:43 +0100

    personaje service

--- src/personaje/personaje.service.ts
+++ src/personaje/personaje.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Personaje } from './entities/personaje.entity';
 
 @Injectable()
-export class PersonajeService {}
+export class PersonajeService {
+
+  private personajes: Personaje[] = [
+    { id: 1, name: 'Gandalf', hp: 100, attack: 20, level: 1 },
+    { id: 2, name: 'Sauron', hp: 30, attack: 5, level: 1 },
+  ];
+
+  findAll(): Personaje[] {
+    return this.personajes;
+  }
+
+  atacar(atacanteId: number, objetivoId: number) {
+    if (atacanteId === objetivoId) {
+      throw new BadRequestException('No puedes atacarte a ti mismo');
+    }
+
+    const atacante = this.personajes.find(c => c.id === Number(objetivoId));
+    const objetivo = this.personajes.find(c => c.id === Number(objetivoId));
+
+    if (!atacante || !objetivo) {
+      throw new NotFoundException('Personaje no encontrado');
+    }
+
+    objetivo.hp = objetivo.hp - atacante.attack;
+    
+    if (objetivo.hp < 0) objetivo.hp = 0;
+
+    return {
+      message: `${atacante.name} atacó a ${objetivo.name}`,
+      objetivoStatus: objetivo 
+    };
+  }
+
+  reiniciarPartida() {
+    this.personajes = [
+      { id: 1, name: 'Gandalf', hp: 100, attack: 20, level: 1 },
+      { id: 2, name: 'Sauron', hp: 30, attack: 5, level: 1 },
+    ];
+    return { message: 'Juego reiniciado.' };
+  }
+
+}

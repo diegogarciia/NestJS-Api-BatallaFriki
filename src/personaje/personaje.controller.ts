d818db1b7007240a21a53f8dd322c1ac1db4b02d
Diego <diegogarciaangel25@gmail.com>
2026-02-15 22:29:35 +0100

    personaje controller con getAll, atacar y reiniciarPartida

--- src/personaje/personaje.controller.ts
+++ src/personaje/personaje.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PersonajeService } from './personaje.service';
 
 @Controller('personajes')
-export class PersonajeController {}
+export class PersonajeController {
+  constructor(private readonly personajeService: PersonajeService) {}
+
+  @Get()
+  getAll() {
+    return this.personajeService.findAll();
+  }
+
+  @Post('atacar')
+  atacar(@Body() body: { atacanteId: number; objetivoId: number }) {
+    return this.personajeService.atacar(body.atacanteId, body.objetivoId);
+  }
+
+  @Post('reiniciarPartida')
+  reiniciarPartida() {
+    return this.personajeService.reiniciarPartida();
+  }
+}

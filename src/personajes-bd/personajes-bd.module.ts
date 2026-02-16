0ec2f214dcdc148b491a5a96585f1fceee9bdd06
Diego <diegogarciaangel25@gmail.com>
2026-02-16 22:14:23 +0100

    importación de PrismaModule

--- src/personajes-bd/personajes-bd.module.ts
+++ src/personajes-bd/personajes-bd.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PersonajesBdService } from './personajes-bd.service';
 import { PersonajesBdController } from './personajes-bd.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PersonajesBdController],
   providers: [PersonajesBdService],
 })

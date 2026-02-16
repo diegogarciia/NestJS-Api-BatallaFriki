a945acaa4357ae4bea03fa6fb9e228219ec0b470
Diego <diegogarciaangel25@gmail.com>
2026-02-16 21:57:31 +0100

    controller personajesBD

--- src/personajes-bd/personajes-bd.controller.ts
+++ src/personajes-bd/personajes-bd.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { PersonajesBdService } from './personajes-bd.service';
 import { CreatePersonajesBdDto } from './dto/create-personajes-bd.dto';
 import { UpdatePersonajesBdDto } from './dto/update-personajes-bd.dto';
@@ -18,17 +18,20 @@ export class PersonajesBdController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.personajesBdService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.personajesBdService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePersonajesBdDto: UpdatePersonajesBdDto) {
-    return this.personajesBdService.update(+id, updatePersonajesBdDto);
+  update(
+    @Param('id', ParseIntPipe) id: number, 
+    @Body() updatePersonajesBdDto: UpdatePersonajesBdDto
+  ) {
+    return this.personajesBdService.update(id, updatePersonajesBdDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.personajesBdService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.personajesBdService.remove(id);
   }
 }

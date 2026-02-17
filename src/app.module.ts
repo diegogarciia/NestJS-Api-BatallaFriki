448568aca903f62b2d4dab155df0a7c2fe7eff26
Diego <diegogarciaangel25@gmail.com>
2026-02-17 10:20:58 +0100

    importaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { PersonajeModule } from './personaje/personaje.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { PersonajesBdModule } from './personajes-bd/personajes-bd.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PersonajeModule, UsersModule, ConfigModule.forRoot({ isGlobal: true }), PersonajesBdModule],
+  imports: [PersonajeModule, UsersModule, ConfigModule.forRoot({ isGlobal: true }), PersonajesBdModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })

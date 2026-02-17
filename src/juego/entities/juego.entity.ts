a6f658ef6c35de470bdce481b7c00b7a8b41a668
Diego <diegogarciaangel25@gmail.com>
2026-02-17 17:27:39 +0100

    juego entity

--- src/juego/entities/juego.entity.ts
+++ src/juego/entities/juego.entity.ts
@@ -1,1 +1,18 @@
-export class Juego {}
+export class Juego {
+  id: string;            
+  jugadorId: number;      
+  
+  nombreJugador: string;
+  vidaActualJugador: number;
+  vidaMaxJugador: number;
+  ataqueJugador: number;
+
+  nombreRival: string;
+  vidaActualRival: number;
+  vidaMaxRival: number;
+  ataqueRival: number;
+
+  turno: 'JUGADOR' | 'RIVAL';
+  finalizado: boolean;
+  ganadorId?: number;     
+}

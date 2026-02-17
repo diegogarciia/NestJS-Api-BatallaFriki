f8c04b46837b3f8d59e6941f4e1ea4019eaa0110
Diego <diegogarciaangel25@gmail.com>
2026-02-17 18:48:58 +0100

    actualizar juego module con Gateway

--- src/juego/juego.module.ts
+++ src/juego/juego.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { JuegoService } from './juego.service';
 import { JuegoController } from './juego.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JuegoGateway } from './juego.gateway';
 
 @Module({
   imports: [PrismaModule],
   controllers: [JuegoController],
-  providers: [JuegoService],
+  providers: [JuegoService, JuegoGateway],
 })
 export class JuegoModule {}

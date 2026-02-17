f5c3d0ccedc5deb6467c170bd9909667ca99874d
Diego <diegogarciaangel25@gmail.com>
2026-02-17 20:15:17 +0100

    importar websocketsModule en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { PersonajesBdModule } from './personajes-bd/personajes-bd.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { JuegoModule } from './juego/juego.module';
+import { WebsocketsModule } from './websockets/websockets.module';
 
 @Module({
-  imports: [PersonajeModule, UsersModule, ConfigModule.forRoot({ isGlobal: true }), PersonajesBdModule, AuthModule, PrismaModule, JuegoModule],
+  imports: [PersonajeModule, UsersModule, ConfigModule.forRoot({ isGlobal: true }), PersonajesBdModule, AuthModule, PrismaModule, JuegoModule, WebsocketsModule],
   controllers: [AppController],
   providers: [AppService],
 })

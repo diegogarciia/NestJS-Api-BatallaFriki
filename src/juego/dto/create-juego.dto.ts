c53393cf1c75e74bc52d6b6b0c9b6d343b76370f
Diego <diegogarciaangel25@gmail.com>
2026-02-17 17:24:43 +0100

    create juego

--- src/juego/dto/create-juego.dto.ts
+++ src/juego/dto/create-juego.dto.ts
@@ -1,1 +1,24 @@
-export class CreateJuegoDto {}
+import { IsInt, IsNotEmpty, IsEnum, IsOptional } from 'class-validator';
+
+export enum TipoOponente {
+  CPU = 'CPU',
+  USUARIO = 'USUARIO',
+}
+
+export class CreateJuegoDto {
+  @IsInt()
+  @IsNotEmpty()
+  personajeJugadorId: number; 
+
+  @IsEnum(TipoOponente)
+  @IsNotEmpty()
+  tipoOponente: TipoOponente; 
+
+  @IsInt()
+  @IsOptional()
+  oponenteId?: number; 
+
+  @IsInt()
+  @IsOptional()
+  personajeOponenteId?: number; 
+}

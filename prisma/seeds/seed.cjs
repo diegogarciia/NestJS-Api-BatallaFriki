f7d3bc34945a8d1f52a0ba15757e7d1aa859f6f8
Diego <diegogarciaangel25@gmail.com>
2026-02-17 13:55:04 +0100

    seed jefe

--- prisma/seeds/seed.cjs
+++ prisma/seeds/seed.cjs
@@ -1,32 +1,23 @@
-require('dotenv/config');
 const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-
 const { seedUsers } = require('./users.seed.cjs');
-const { seedPosts } = require('./posts.seed.cjs');
-const { seedRandomPosts } = require('./posts-random.seed.cjs');
+const { seedCharacters } = require('./characters.seed.cjs');
 
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({
-    connectionString: process.env.DATABASE_URL || "postgresql://postgres:Chubaca2025@localhost:5432/ejemploPrisma?schema=public"
-  }),
-});
+const prisma = new PrismaClient();
 
 async function main() {
-  console.log('🌱 Starting database seed...');
-
+  console.log('Iniciando el sembrado de datos...');
+  
   await seedUsers(prisma);
-  await seedPosts(prisma);
-
-  // Seed aleatorio extra
-  await seedRandomPosts(prisma, 5); // 5 posts aleatorios por usuario
-
-  console.log('🌱 Database seed completed');
+  
+  await seedCharacters(prisma);
+  
+  console.log('Proceso finalizado con éxito.');
 }
 
 main()
   .catch((e) => {
-    console.error('❌ Seed failed', e);
+    console.error('Error en el seed:', e);
+    process.exit(1);
   })
   .finally(async () => {
     await prisma.$disconnect();
